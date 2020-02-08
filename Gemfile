6c7a8566204460ea3dbb3f307f11a4222bd60409
Fei Wang <feiwangsioc@gmail.com>
2020-02-08 03:56:39 +0000

    UI and select2

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "rails-i18n"
 
 gem "devise-i18n"
 
+gem "select2-rails"
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/models/event.rb
+++ app/models/event.rb
@@ -16,5 +16,10 @@ class Event < ApplicationRecord
     def generate_friendly_id
       self.friendly_id ||= SecureRandom.uuid
     end
+    
+    STATUS = ["draft", "public", "private"]
+    validates_inclusion_of :status, :in => STATUS
+    
+    belongs_to :category, :optional => true
 
 end
